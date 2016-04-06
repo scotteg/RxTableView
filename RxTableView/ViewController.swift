7c4325f2c1e9b52249c8a18204b20a59d2a96e35
Scott Gardner <scott.gardner@mac.com>
2016-04-06 09:52:21 -0500

    Initial commit

---
+++ RxTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxTableView
+//
+//  Created by Scott Gardner on 4/6/16.
+//  Copyright © 2016 Scott Gardner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
